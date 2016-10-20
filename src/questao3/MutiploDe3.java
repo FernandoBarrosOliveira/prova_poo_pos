6962a7d7074964b8bc31796181fdb95d48e239d9
Fernando Barros <fernando.barros.oliveira@gmail.com>
2016-10-19 23:21:12 -0300

    Commit questao 4

---
+++ src/questao3/MutiploDe3.java
@@ -0,0 +1,40 @@
+package questao3;
+
+import javax.print.attribute.standard.NumberOfDocuments;
+
+public class MutiploDe3 {
+	
+	private static boolean numeroMutiplo3(int numero){
+		return numero%3 ==0; 		
+	}
+	
+	static void testarMutiplosDe3(int [][] matriz) throws NaoMultiploDe3Exception{ 
+		for(int linha = 0 ; linha < matriz.length; linha++ ){
+			for(int coluna = 0 ; coluna < matriz[linha].length; coluna++ ){
+				if (! numeroMutiplo3(matriz[linha][coluna])){
+					throw new NaoMultiploDe3Exception(linha, coluna);
+				}
+			}				
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		//inicializar a matriz
+		int [][] matriz = {
+			{3,6},
+			{9,2}
+		};
+	
+		try{
+			testarMutiplosDe3(matriz);						
+		}catch (NaoMultiploDe3Exception e){
+			System.out.println(e.getMessage()+ ":  linha = "+e.getLinha() +" / coluna =  " + e.getColuna());
+		}
+		
+		
+
+	}
+
+}
---
+++ src/questao3/NaoMultiploDe3Exception.java
@@ -0,0 +1,22 @@
+package questao3;
+
+public class NaoMultiploDe3Exception extends Exception {
+	private int linha;
+	private int coluna;
+	
+	public int getLinha() {
+		return linha;
+	}
+	public int getColuna() {
+		return coluna;
+	}
+	
+	public NaoMultiploDe3Exception(int linha, int coluna){
+		super("Erro elemento não é mutiplo de 3");
+		this.linha = linha;
+		this.coluna = coluna;		
+	}
+	
+	
+	
+}
